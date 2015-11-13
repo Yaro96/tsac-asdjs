606bde9a44c8aed9fdf66c048a6077680236c80e
Yaro96 <yaro96@hotmail.com>
2015-11-13 15:40:34 +0100

    funzioni

--- homework_02.js
+++ homework_02.js
@@ -4,104 +4,53 @@
 Esercizio 1
 Dato un array di interi, restituire la loro somma fino a che non viene ritrovato un valore negativo
 */
-// Iterative
-// Spazio: O(1) 
-// Tempo: O(n)
-function sumwhileneg(myarray) {
-    var tot = 0;
-    for(i = 0; i < myarray.length; ++i) {
-        var x = myarray[i];
-        if (x > 0) {
-            tot += x;
-        } else {
-            return tot;
-        }
-    }
-    return tot;
+
+function ex_1_F(myarray) 
+{
+        var already=false;
+        var tot=myarray.reduce(
+                                    function(a,x)
+                                    {
+                                        if(x>0 && !already)
+                                            a+=x;
+                                        else
+                                            already=true;
+                                         return a;
+                                    },0
+                              )
+        return tot;
 }
 
-
-// Ricorsive
-// Spazio: O(n) 
-// Tempo: O(n)
-function sumwhilenegR(myarray) {
-    if (myarray.length == 0 || myarray[0] < 0) {
-        return 0;
-    } else {
-        return myarray[0] + sumwhilenegR(myarray.slice(1));
-    }
-}
-
-// Per Tiziano.
-function ex_1_I(myarray) {
-    return sumwhileneg(myarray);
-}
-
-function ex_1_R(myarray) {
-    return sumwhilenegR(myarray);
-}
 
 /*
 Esercizio 2
 Dato un numero n, restituire la somma dei primi n numeri interi positivi dispari
 */
-// Spazio: O(1) 
-// Tempo: O(n)
-function firstOdd(n) {
-    var tot = 0;
-    for(i = 0; i < n; ++i) {
-        tot += 1 + 2 * i;
+
+function ex_2_F(n)
+{
+    var array=[];
+    j=1;
+    for(i=0;i<n;i++)
+    {
+        array[i]=j;
+        j+=2;
     }
-    return tot;
+
+    return array.reduce(function(a,x){return a+x;},0);
+
 }
 
-
-// Spazio: O(n) 
-// Tempo: O(n)
-function firstOddR(n) {
-    if (n == 0) {
-        return 0;
-    } else {
-        return 2 * n - 1 + firstOddR(n - 1);
-    }
-}
-
-
-
-// Per Tiziano.
-function ex_2_I(x) {
-    return firstOdd(x);
-}
-
-function ex_2_R(x) {
-    return firstOddR(x);
-}
 
 /*
 Esercizio 3
 Dato un array di 10 elementi, calcolarne la media
 */
 
-// Spazio: O(1) 
-// Tempo: O(1)
-function ex_3_I(array)
+function ex_3_F(array)
 {
-    var media=0;
-    for(i=0;i<10;i++)
-    {
-        media=media+array[i];
-    }
-    return media/10;
-}
-
-// Spazio: O(n) 
-// Tempo: O(n)
-function ex_3_R(array)
-{
-    if(array.length<=0)
-        return 0;
-    else
-        return (array[0]/10+ex_3_R(array.slice(1)));
+    var tot=array.reduce(function(a,x){return a+x;},0);
+    return tot/10;
 }
 
 /*
@@ -110,8 +59,11 @@ Dato un intervallo [a, b] con a e b due interi positivi, restituire la somma di 
 compresi all’interno dell’intervallo, estremi inclusi. Nel caso che b fosse minore di a,
 calcolare la somma nell’intervallo [b,a]*/
 
-// Spazio: O(1) 
-// Tempo: O(n)
+function ex_4_F(a,b)
+{
+
+}
+
 function ex_4_I(a,b)
 {
     if(a>b)
